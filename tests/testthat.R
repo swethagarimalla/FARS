05a2e7f0b7bc7ef496ff7d062ab74cb2a0567cb2
swethagarimalla <swetha.garimalla@gatech.edu>
2018-06-12 20:34:33 -0400

    shifted testthat.R into another folder using devtools::use_testthat()

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,11 @@
 library(testthat)
+library(FARS)
 library(mapdata)
-library(farsdata)
 
 test_that('This map function works!', {
   fn <- make_filename(2013) 
   expect_that(fn, equals("accident_2013.csv.bz2"))
   fars_mapping <- fars_map_state(13, 2013)
   expect_that(fars_mapping, is_null())
-  }
+}
 )
