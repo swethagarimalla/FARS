1147d5e5b1de63e3dd10e9395b8614a79ed91980
swethagarimalla <swetha.garimalla@gatech.edu>
2018-06-12 20:28:57 -0400

    added ~ to n and year

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -87,7 +87,7 @@ fars_read_years <- function(years) {
                 file <- make_filename(year)
                 tryCatch({
                         dat <- fars_read(file)
-                        dplyr::mutate(dat, year = year) %>% 
+                        dplyr::mutate(dat, year = ~ year) %>% 
                                 dplyr::select(~ MONTH, ~ year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
@@ -123,7 +123,7 @@ fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
         dplyr::bind_rows(dat_list) %>% 
                 dplyr::group_by( ~ year, ~ MONTH) %>% 
-                dplyr::summarize(n = ~ n()) %>%
+                dplyr::summarize(~ n = ~ n()) %>%
                 tidyr::spread(year, n)
 }
 
