7696254d2c396e81b54fa55ea26da565f59f4fe6
swethagarimalla <swetha.garimalla@gatech.edu>
2018-06-12 20:25:21 -0400

    modify undefined global variables

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -88,7 +88,7 @@ fars_read_years <- function(years) {
                 tryCatch({
                         dat <- fars_read(file)
                         dplyr::mutate(dat, year = year) %>% 
-                                dplyr::select(MONTH, year)
+                                dplyr::select(~ MONTH, ~ year)
                 }, error = function(e) {
                         warning("invalid year: ", year)
                         return(NULL)
@@ -122,8 +122,8 @@ fars_read_years <- function(years) {
 fars_summarize_years <- function(years) {
         dat_list <- fars_read_years(years)
         dplyr::bind_rows(dat_list) %>% 
-                dplyr::group_by(year, MONTH) %>% 
-                dplyr::summarize(n = n()) %>%
+                dplyr::group_by( ~ year, ~ MONTH) %>% 
+                dplyr::summarize(n = ~ n()) %>%
                 tidyr::spread(year, n)
 }
 
