98988b831ac01b90866c9537e99a34f178802ce5
swethagarimalla <swetha.garimalla@gatech.edu>
2018-06-12 20:58:17 -0400

    modified fars_function

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -161,22 +161,22 @@ fars_summarize_years <- function(years) {
 #' @export
 
 fars_map_state <- function(state.num, year) {
-        filename <- make_filename(year)
-        data <- fars_read(filename)
-        state.num <- as.integer(state.num)
-
-        if(!(state.num %in% unique(data$STATE)))
-                stop("invalid STATE number: ", state.num)
-        data.sub <- dplyr::filter(data, STATE == state.num)
-        if(nrow(data.sub) == 0L) {
-                message("no accidents to plot")
-                return(invisible(NULL))
-        }
-        is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
-        is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
-        with(data.sub, {
-                maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
-                          xlim = range(LONGITUD, na.rm = TRUE))
-                graphics::points(LONGITUD, LATITUDE, pch = 46)
-        })
+  filename <- make_filename(year)
+  data <- fars_read(filename)
+  state.num <- as.integer(state.num)
+  
+  if(!(state.num %in% unique(data$STATE)))
+    stop("invalid STATE number: ", state.num)
+  data.sub <- dplyr::filter_(data, ~ STATE == state.num)
+  if(nrow(data.sub) == 0L) {
+    message("no accidents to plot")
+    return(invisible(NULL))
+  }
+  is.na(data.sub$LONGITUD) <- data.sub$LONGITUD > 900
+  is.na(data.sub$LATITUDE) <- data.sub$LATITUDE > 90
+  with(data.sub, {
+    maps::map("state", ylim = range(LATITUDE, na.rm = TRUE),
+              xlim = range(LONGITUD, na.rm = TRUE))
+    graphics::points(LONGITUD, LATITUDE, pch = 46)
+  })
 }
