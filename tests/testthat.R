1e293bcb856b956647b8393e89579ddac85fa950
swethagarimalla <swetha.garimalla@gatech.edu>
2018-06-12 20:59:46 -0400

    modified tests

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,8 +3,6 @@ library(FARS)
 library(mapdata)
 
 test_that('This map function works!', {
-  fn <- make_filename(2014) 
-  expect_that(fn, equals("accident_2014.csv.bz2"))
   fars_mapping <- fars_map_state(13, 2013)
   expect_that(fars_mapping, is_null())
 }
